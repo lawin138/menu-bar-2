8fbff2672e333bf0c6de5bffa728eb746c099ba3
lawin 138 <lawin42600@gmail.com>
2025-03-24 11:49:51 +0800

    changed, nothing

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 let btn1 =document.querySelector('#btn1');
 let ul1 =document.querySelector('.ul1');
+
 let btn2 =document.querySelector('#btn2');
 let ul2 =document.querySelector('.ul2');
+
 let btn3 =document.querySelector('#btn3');
 let ul3 =document.querySelector('.ul3');
 
 
-btn1.addEventListener('click',e=>{
-    e.preventDefault()    
+btn1.addEventListener('click',function(){
     ul1.classList.toggle('hide')
-})
-    
-       
+})      
 btn2.addEventListener('click', function(){
     ul2.classList.toggle('hide1')
 });
